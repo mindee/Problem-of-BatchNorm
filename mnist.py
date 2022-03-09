c538635c94a80ae2eed654dcc9b89ce3865f2600
charlesmindee <charles@mindee.co>
2022-03-09 11:34:29 +0100

    fix: modes

--- mnist.py
+++ mnist.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from prometheus_client import Enum
 import torch
 from pytorch_lightning import LightningModule, Trainer
 from torch import nn
@@ -8,6 +9,7 @@ from torch.utils.data import DataLoader, random_split
 from torchmetrics import Accuracy
 from torchvision import transforms
 from torchvision.datasets import MNIST
+from tqdm import tqdm
 
 
 PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
@@ -15,8 +17,37 @@ AVAIL_GPUS = min(1, torch.cuda.device_count())
 BATCH_SIZE = 128 if AVAIL_GPUS else 64
 
 
+def deactivate_ema(model):
+    for m in model.modules():
+        if isinstance(m, nn.BatchNorm2d):
+            m.track_running_stats = False
+            m._saved_running_mean, m.running_mean = m.running_mean, None
+            m._saved_running_var, m.running_var = m.running_var, None
+
+
+def activate_ema(model):
+    for m in model.modules():
+        if isinstance(m, nn.BatchNorm2d):
+            m.track_running_stats = True
+            m.running_mean = m._saved_running_mean
+            m.running_var = m._saved_running_var
+
+
+def calculate_estimates(model):
+    with torch.no_grad():
+        for data in tqdm(model.train_dataloader()):
+            model(data[0])
+
+
+class Modes(int, Enum):
+    no_BN = 0
+    basic_BN = 1
+    almost_smart_BN = 2
+    smart_BN = 3
+
+
 class LitMNIST(LightningModule):
-    def __init__(self, data_dir=PATH_DATASETS, hidden_size=8, learning_rate=5e-3):
+    def __init__(self, data_dir=PATH_DATASETS, hidden_size=8, learning_rate=5e-3, mode=Modes.smart_BN):
 
         super().__init__()
 
@@ -37,16 +68,31 @@ class LitMNIST(LightningModule):
         )
 
         # Define PyTorch model
-        self.model = nn.Sequential(
-            nn.Conv2d(channels, hidden_size, 3, 2, 1),
-            nn.BatchNorm2d(hidden_size, eps=1e-05, momentum=0.9, affine=True, track_running_stats=True),
-            nn.ReLU(),
-            nn.Conv2d(hidden_size, hidden_size, 3, 2, 1),
-            nn.BatchNorm2d(hidden_size, eps=1e-05, momentum=0.9, affine=True, track_running_stats=True),
-            nn.ReLU(),
-            nn.Conv2d(hidden_size, self.num_classes, 3, 2, 1),
-            nn.BatchNorm2d(self.num_classes, eps=1e-05, momentum=0.9, affine=True, track_running_stats=True),
-        )
+        if mode == Modes.no_BN:
+            self.model = nn.Sequential(
+                nn.Conv2d(channels, hidden_size, 3, 2, 1),
+                nn.ReLU(),
+                nn.Conv2d(hidden_size, hidden_size, 3, 2, 1),
+                nn.ReLU(),
+                nn.Conv2d(hidden_size, self.num_classes, 3, 2, 1),
+            )
+        else:
+            self.model = nn.Sequential(
+                nn.Conv2d(channels, hidden_size, 3, 2, 1),
+                nn.BatchNorm2d(hidden_size, eps=1e-05, momentum=0.9,
+                               affine=True, track_running_stats=True),
+                nn.ReLU(),
+                nn.Conv2d(hidden_size, hidden_size, 3, 2, 1),
+                nn.BatchNorm2d(hidden_size, eps=1e-05, momentum=0.9,
+                               affine=True, track_running_stats=True),
+                nn.ReLU(),
+                nn.Conv2d(hidden_size, self.num_classes, 3, 2, 1),
+                nn.BatchNorm2d(self.num_classes, eps=1e-05, momentum=0.9,
+                               affine=True, track_running_stats=True),
+            )
+
+        if mode == Modes.smart_BN or mode == Modes.almost_smart_BN:
+            deactivate_ema(self.model)
 
         self.train_accuracy = Accuracy()
         self.val_accuracy = Accuracy()
@@ -69,11 +115,10 @@ class LitMNIST(LightningModule):
         for out in training_step_outputs:
             loss.append(out["loss"].cpu().numpy())
             acc.append(out["acc"].cpu().numpy())
-        
+
         # Calling self.log will surface up scalars for you in TensorBoard
         self.log("train_loss", np.mean(loss), prog_bar=True)
         self.log("train_acc", np.mean(acc), prog_bar=True)
-        
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
@@ -108,12 +153,15 @@ class LitMNIST(LightningModule):
 
         # Assign train/val datasets for use in dataloaders
         if stage == "fit" or stage is None:
-            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
-            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
+            mnist_full = MNIST(self.data_dir, train=True,
+                               transform=self.transform)
+            self.mnist_train, self.mnist_val = random_split(
+                mnist_full, [55000, 5000])
 
         # Assign test dataset for use in dataloader(s)
         if stage == "test" or stage is None:
-            self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
+            self.mnist_test = MNIST(
+                self.data_dir, train=False, transform=self.transform)
 
     def train_dataloader(self):
         return DataLoader(self.mnist_train, batch_size=BATCH_SIZE, shuffle=True)
@@ -122,17 +170,42 @@ class LitMNIST(LightningModule):
         return DataLoader(self.mnist_val, batch_size=BATCH_SIZE, shuffle=True)
 
     def test_dataloader(self):
-        return DataLoader(self.mnist_test, batch_size=BATCH_SIZE, shuffle=True)
+        return DataLoader(self.mnist_test, batch_size=1, shuffle=True)
 
 
-model = LitMNIST()
+if __name__ == "__main__":
 
-trainer = Trainer(
-    gpus=AVAIL_GPUS,
-    max_epochs=10,
-    progress_bar_refresh_rate=20,
-)
+    from argparse import ArgumentParser
 
-trainer.fit(model)
+    parser = ArgumentParser()
+    parser.add_argument("mode", type=int)
+    args = parser.parse_args()
 
-trainer.test()
+    if args.mode == 0:
+        mode = Modes.no_BN
+    elif args.mode == 1:
+        mode = Modes.basic_BN
+    elif args.mode == 2:
+        mode = Modes.almost_smart_BN
+    elif args.mode == 3:
+        mode = Modes.smart_BN
+    else:
+        raise ValueError("Mode not allowed, select 0, 1, 2 or 3 for no batchnorm, " +
+                         "basic batchnorm, almost smart & smart batchnorm respectively")
+
+    model = LitMNIST(mode=mode)
+
+    trainer = Trainer(
+        gpus=AVAIL_GPUS,
+        max_epochs=10,
+        progress_bar_refresh_rate=20,
+    )
+
+    trainer.fit(model)
+
+    if mode == Modes.smart_BN or mode == Modes.almost_smart_BN:
+        activate_ema(model)
+        if mode == Modes.smart_BN:
+            calculate_estimates(model)
+
+    trainer.test(model)
--- plot.py
+++ plot.py
@@ -3,30 +3,45 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv("/home/laptopmindee/Documents/batchnorm.csv")
+def plot_train_val():
+    # Import dataframe
+    df = pd.read_csv("/home/laptopmindee/Documents/exp_batch.csv")
 
-# Split and rename dataframes
-df_True = df[["Step", "True-train", "True-val"]]
-df_True = df_True.rename(columns={'True-train': 'Train', 'True-val': 'Val'})
-df_False = df[["Step", "False-train", "False-val"]]
-df_False = df_False.rename(columns={'False-train': 'Train', 'False-val': 'Val'})
-df_Sans = df[["Step", "Sans-train", "Sans-val"]]
-df_Sans = df_Sans.rename(columns={'Sans-train': 'Train', 'Sans-val': 'Val'})
+    # Split and rename dataframes
+    df_0 = df[["step", "0-train", "0-val"]]
+    df_0 = df_0.rename(columns={'0-train': 'Train', '0-val': 'Val'})
+    df_1 = df[["step", "1-train", "1-val"]]
+    df_1 = df_1.rename(columns={'1-train': 'Train', '1-val': 'Val'})
+    df_3 = df[["step", "3-train", "3-val"]]
+    df_3 = df_3.rename(columns={'3-train': 'Train', '3-val': 'Val'})
 
-dfm_True = df_True.melt('Step', var_name='exp', value_name='Accuracy')
-dfm_False = df_False.melt('Step', var_name='exp', value_name='Accuracy')
-dfm_Sans = df_Sans.melt('Step', var_name='exp', value_name='Accuracy')
+    dfm_0 = df_0.melt('step', var_name='exp', value_name='Accuracy')
+    dfm_1 = df_1.melt('step', var_name='exp', value_name='Accuracy')
+    dfm_3 = df_3.melt('step', var_name='exp', value_name='Accuracy')
 
-sns.set_theme()
+    # Configure plot
+    sns.set_theme()
+    fig, axes = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(15, 5))
+    fig.suptitle("Train & Val accuracy on 10 epochs (MNIST) with modes 0, 1 and 3")
+    axes[0].set_title("mode 0: without BN")
+    axes[1].set_title("mode 1: with basic BN")
+    axes[2].set_title("mode 3: with smart BN")
 
-fig, axes = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(15,5))
-fig.suptitle("Train & Val accuracy on 10 epochs (MNIST) with and without BatchNorm")
-axes[0].set_title("With running average")
-axes[1].set_title("Without running average")
-axes[2].set_title("Without BatchNorm")
+    # Plot
+    sns.lineplot(data=dfm_0, x="step", y="Accuracy", hue="exp", palette="viridis", ax=axes[0])
+    sns.lineplot(data=dfm_1, x="step", y="Accuracy", hue="exp", palette="viridis", ax=axes[1])
+    sns.lineplot(data=dfm_3, x="step", y="Accuracy", hue="exp", palette="viridis", ax=axes[2])
+    sns.move_legend(axes[0], loc='lower right')
+    sns.move_legend(axes[1], loc='lower right')
+    sns.move_legend(axes[2], loc='lower right')
+    plt.show()
 
-sns.lineplot(data=dfm_True, x="Step", y="Accuracy", hue="exp", palette="flare", ax=axes[0])
-sns.lineplot(data=dfm_False, x="Step", y="Accuracy", hue="exp", palette="flare", ax=axes[1])
-sns.lineplot(data=dfm_Sans, x="Step", y="Accuracy", hue="exp", palette="flare", ax=axes[2])
-plt.show()
 
+def plot_test():
+    fig, _ = plt.subplots()
+    fig.suptitle("Test accuracy for each mode")
+    x = ["0", "1", "2", "3"]
+    y = [0.9070, 0.9103, 0.101199, 0.919900]
+    clrs4 = ["palegreen", "skyblue", "salmon", "tomato"]
+    sns.barplot(x=x, y=y, palette=clrs4)
+    plt.show()
